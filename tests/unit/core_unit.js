eb303d1a9af1f770c5c01cd1a5fc2b80ae5e6cca
Marc Grabanski <m@marcgrabanski.com>
2009-04-22 03:05:43 -0500

    added test suite, re-orged files

---
+++ tests/unit/core_unit.js
@@ -0,0 +1,8 @@
+module("Calendar Engine");
+
+test("Basic test", function() {
+	expect(3);
+	ok(true, 'foo is true');
+	equals(1, true, 'true is 1');
+	same(1, 1, 'true is 1');
+});
---
+++ tests/unit/date_unit.js
@@ -0,0 +1,1 @@
+module("Date Engine");
